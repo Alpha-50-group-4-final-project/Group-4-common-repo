5a55bc613635b34eb0bee65893100de99125b511
Aneliya Boneva <132662622+AneliyaYaYaYa@users.noreply.github.com>
2023-09-20 12:14:00 +0300

    amend RegistrationTest part2

--- New framework/TestAutomationFramework/src/test/java/WEare/RegistrationTests.java
+++ New framework/TestAutomationFramework/src/test/java/WEare/RegistrationTests.java
@@ -1,14 +1,48 @@
 package WEare;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class RegistrationTests extends  BaseTest {
 
-    @ParameterizedTest
-    @CsvSource({ "Bork,123Password_","Dinko,123."})
-    public void registerNewUserTest_When_validCredentials_arePassed(String username,String password) {
-        registerNewUser.registerNewUser(username, password);
+    @Test
+    public void registerNewUserTest_when_validCredentialsPassed() {
+        registerNewUser.registerNewUser(usernameRandom, passwordRandom);
         registerNewUser.assertUserCreated();
+    }
+    @ParameterizedTest
+    @CsvSource({ "а", "aa", "itIsBoundryUpperlevel"})
+    public void registerNewUser_when_invalidUsernamePassed(String username){
+        registerNewUser.registerNewUser(username, passwordRandom);
+        registerNewUser.assertUserCreated();
+
+    }
+    @Test
+    public void registerNewUser_when_emptyUsernamePassed(){
+        registerNewUser.registerNewUser("",passwordRandom);
+        registerNewUser.assertUserCreated();
+    }
+    @Test
+    public void registerNewUser_when_invalidEmailPassed(String email){
+    }
+    @Test
+    public void registerNewUser_when_emptyEmailPassed(){
+
+    }
+    @ParameterizedTest
+    @CsvSource({ "12345", "aa", "itIsBoundryUpperlevel"})
+    public void registerNewUser_when_invalidPasswordPassed(String password){
+    }
+    @Test
+    public void registerNewUser_when_emptyPasswordPassed(){
+        registerNewUser.registerNewUser(usernameRandom, "");
+        registerNewUser.assertUserCreated();
+    }
+
+    //invalid dropdown? could it be?
+    @Test
+    public void registerNewUser_when_invalidDropdownPassed(){
+
     }
 }
