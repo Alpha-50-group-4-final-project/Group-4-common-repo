f238925910d3d922ac7ab53d7635d8c999dfc2a1
BorisYurukov <boris.yurukov.a50@learn.telerikacademy.com>
2023-09-24 16:11:58 +0300

    Minor changes on ui_map.properties аnd some parts of  new tests are added.Faker added

--- New framework/TestAutomationFramework/src/test/java/WEare/privatepart/SendAndAcceptFriendRequests.java
+++ New framework/TestAutomationFramework/src/test/java/WEare/privatepart/SendAndAcceptFriendRequests.java
@@ -6,56 +6,66 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.plaf.PanelUI;
+
 public class SendAndAcceptFriendRequests extends BaseTest {
 
-    public static  String FIRST_USER ;
-    public static  String SECOND_USER ;
+    public static String FIRST_USER;
+    public static String SECOND_USER;
+
+    private static String firstUserFirstName;
+    private static String secondUserFirstName;
+
     public static final String USERS_PASSWORD = "pass_123";
 
     @BeforeAll
-    public static void testSetUp(){
-        FIRST_USER= faker.name().firstName();
-        SECOND_USER= faker.name().firstName();
+    public static void testSetUp() {
+        FIRST_USER = faker.name().firstName();
+        SECOND_USER = faker.name().firstName();
+        firstUserFirstName = faker.name().firstName();
+        secondUserFirstName = faker.name().firstName();
         registerUser(FIRST_USER, USERS_PASSWORD);
         registerUser(SECOND_USER, USERS_PASSWORD);
-        login(FIRST_USER,USERS_PASSWORD);
+        login(FIRST_USER, USERS_PASSWORD);
         editProfilePage.navigateToHomePage();
         editProfilePage.navigateToEditProfileMenu();
-        userSetUP("JonYBoy","Snow","01-01-1970");
+        userSetUP(firstUserFirstName, "Snow", "01-01-1970");
         loginPage.clickOnLogOutButton();
-        login(SECOND_USER,USERS_PASSWORD);
+        login(SECOND_USER, USERS_PASSWORD);
         editProfilePage.navigateToHomePage();
         editProfilePage.navigateToEditProfileMenu();
-        userSetUP("Arya","Stark","01-01-1970");
+        userSetUP(secondUserFirstName, "Stark", "01-01-1970");
     }
+
     @AfterAll
-    public static void  clean(){
+    public static void clean() {
         loginPage.clickOnLogOutButton();
     }
 
     @Test
-    public void sendConnectRequest(){
+    public void sendConnectRequest() {
         editProfilePage.navigateToHomePage();
-        homePage.typeIntoNameSearchBox("Jon");
+        homePage.typeIntoNameSearchBox(firstUserFirstName);
         homePage.clickOnSearchButton();
-        searchingPage.seeCurrentUserProfileByName("Jon");
+        searchingPage.seeCurrentUserProfileByName(firstUserFirstName);
         searchingPage.clickOnConnectButton();
         searchingPage.assertRequestIsSend();
     }
+
     @Test
-    public void acceptConnectRequest(){
+    public void acceptConnectRequest() {
         editProfilePage.navigateToHomePage();
-        homePage.typeIntoNameSearchBox("JonYBoy");
+        homePage.typeIntoNameSearchBox(firstUserFirstName);
         homePage.clickOnSearchButton();
-        searchingPage.seeCurrentUserProfileByName("JonYBoy");
+        searchingPage.seeCurrentUserProfileByName(firstUserFirstName);
         searchingPage.clickOnConnectButton();
         searchingPage.assertRequestIsSend();
         loginPage.clickOnLogOutButton();
-        login(FIRST_USER,USERS_PASSWORD);
+        login(FIRST_USER, USERS_PASSWORD);
         editProfilePage.navigateToHomePage();
         homePage.navigateToPersonalProfileButton();
         searchingPage.clickOnNewFriendRequestButton();
-        searchingPage.approveRequestByUserFirstName("Arya");
+        searchingPage.approveRequestByUserFirstName(secondUserFirstName);
 
     }
 }
