0744b55e6b69293cfa02af7271cf9baa6d5b7216
Aneliya Boneva <132662622+AneliyaYaYaYa@users.noreply.github.com>
2023-10-06 12:00:45 +0300

    refactoring RegistrationTests and RegsitartionPage
    compiling all tests into 5 with Parameterized

--- New framework/TestAutomationFramework/src/test/java/WEare/publicpart/RegistrationTests.java
+++ New framework/TestAutomationFramework/src/test/java/WEare/publicpart/RegistrationTests.java
@@ -1,7 +1,6 @@
 package WEare.publicpart;
 
 import WEare.BaseTest;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -19,11 +18,10 @@ public class RegistrationTests extends BaseTest {
         registrationPage.selectCategoryField();
         registrationPage.clickRegistryButton();
         registrationPage.assertUserCreatedWithWelcomeText();
-        LOGGER.info("New user was registered successfully.");
         usernames.add(usernameRandom);
     }
     @ParameterizedTest
-    @CsvSource({ "а",})
+    @CsvSource({ "а", "''"})
     public void registerNewUser_when_invalidUsernamePassed(String username){
         homePage.navigateToRegisterPage();
         registrationPage.fillUsernameField(username);
@@ -32,45 +30,22 @@ public class RegistrationTests extends BaseTest {
         registrationPage.selectCategoryField();
         registrationPage.clickRegistryButton();
         registrationPage.validateRegistryNotSuccessful();
-        LOGGER.info("Expected error message was shown");
     }
-    @Test
-    public void registerNewUser_when_emptyUsernamePassed(){
-        homePage.navigateToRegisterPage();
-        registrationPage.fillUsernameField("");
-        registrationPage.fillEmailField(usernameRandom);
-        registrationPage.fillPasswordFields(passwordRandom);
-        registrationPage.selectCategoryField();
-        registrationPage.clickRegistryButton();
-        registrationPage.validateRegistryNotSuccessful();
-        LOGGER.info("Expected error message was shown");
-        System.out.println();
-    }
-    //notReadY:
-    @Test
-    public void registerNewUser_when_invalidEmailPassed(){
-        homePage.navigateToRegisterPage();
-        registrationPage.fillUsernameField(usernameRandom);
-        registrationPage.fillEmailField("");
-        registrationPage.fillPasswordFields(passwordRandom);
-        registrationPage.selectCategoryField();
-        registrationPage.clickRegistryButton();
-        registrationPage.validateRegistryNotSuccessful();
-        LOGGER.info("Expected error message was shown");
-    }
-    @Test
-    public void registerNewUser_when_emptyEmailPassed(){
-        homePage.navigateToRegisterPage();
-        registrationPage.fillUsernameField(usernameRandom);
-        registrationPage.leaveEmailEmpty("");
-        registrationPage.fillPasswordFields(passwordRandom);
-        registrationPage.selectCategoryField();
-        registrationPage.clickRegistryButton();
-        registrationPage.validateRegistryNotSuccessful();
-        LOGGER.info("Expected error message was shown");
-    }
+
     @ParameterizedTest
-    @CsvSource({ "12345", "01234567891011121314"})
+    @CsvSource({ "anna@sd.comsa", "''"})
+    public void registerNewUser_when_invalidEmailPassed(String email){
+        homePage.navigateToRegisterPage();
+        registrationPage.fillUsernameField(usernameRandom);
+        registrationPage.fillEmailField(email);
+        registrationPage.fillPasswordFields(passwordRandom);
+        registrationPage.selectCategoryField();
+        registrationPage.clickRegistryButton();
+        registrationPage.validateRegistryNotSuccessful();
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "12345", "''"})
     public void registerNewUser_when_invalidPasswordPassed(String password){
         homePage.navigateToRegisterPage();
         registrationPage.fillUsernameField(usernameRandom);
@@ -79,18 +54,6 @@ public class RegistrationTests extends BaseTest {
         registrationPage.selectCategoryField();
         registrationPage.clickRegistryButton();
         registrationPage.validateRegistryNotSuccessful();
-        LOGGER.info("Expected error message was shown");
-    }
-    @Test
-    public void registerNewUser_when_emptyPasswordPassed(){
-        homePage.navigateToRegisterPage();
-        registrationPage.fillUsernameField(usernameRandom);
-        registrationPage.fillEmailField(usernameRandom);
-        registrationPage.fillPasswordFields("");
-        registrationPage.selectCategoryField();
-        registrationPage.clickRegistryButton();
-        registrationPage.validateRegistryNotSuccessful();
-        LOGGER.info("Expected error message was shown");
     }
     @Test
     public void registerNewUser_when_optionalField_emptyDropdownPassed(){
