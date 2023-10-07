6c0543d3b5c391fbdf1502cddb35792b048cfe43
BorisYurukov <133022686+BorisYurukov@users.noreply.github.com>
2023-10-07 10:39:11 +0300

    Old framework folder deleted
    Old framework folder deleted

--- New framework/TestAutomationFramework/src/test/java/WEare/publicpart/LoginTests.java
+++ New framework/TestAutomationFramework/src/test/java/WEare/publicpart/LoginTests.java
@@ -2,10 +2,7 @@ package WEare.publicpart;
 
 import WEare.BaseTest;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
--- Old frameWork/TestAutomationFramework/src/main/java/pages/WEare/WEarePerosnalProfilePage.java
+++
@@ -1,4 +0,0 @@
-package pages.WEare;
-
-public class WEarePerosnalProfilePage {
-}
