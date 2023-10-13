915874817a64239d4c48313eb0ba2520dca1fed7
BorisYurukov <133022686+BorisYurukov@users.noreply.github.com>
2023-10-13 11:25:49 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	REST-Assured-WEare/src/test/java/weare/commentcontrollers/CreateCommentTests.java
    #	REST-Assured-WEare/src/test/java/weare/postcontrollers/CreatePostsTests.java
    #	REST-Assured-WEare/src/test/java/weare/usercotrollers/UpgradeUsersTests.java
    #	REST-Assured-WEare/src/test/resources/main-suite.xml

---
+++ WEare-SeleniumWebDriver-tests/SeleniumWebDriver-automation-framework/src/test/java/WEare/privatepart/CommentsTests.java
@@ -0,0 +1,77 @@
+package WEare.privatepart;
+
+import WEare.BaseTest;
+import com.telerikacademy.testframework.api.PostModel;
+import org.junit.jupiter.api.*;
+
+
+
+public class CommentsTests extends BaseTest {
+    //private static final String VALID_COMMENT_MESSAGE = "This is a valid comment made by Selenium WebDriver";
+
+    @BeforeAll
+    public static void testSetup() {
+        api.registerUser(usernameRandom, passwordRandom);
+        apiPost = api.createPost(usernameRandom, passwordRandom, POST_MESSAGE);
+        System.out.println(apiPost.postId);
+        var two = api.createComment(usernameRandom, passwordRandom, POST_MESSAGE, apiPost.postId);
+        login(usernameRandom, passwordRandom);
+        postsPage.navigateToPage();
+    }
+
+    @AfterAll
+    public static void returnHome() {
+        homePage.navigateToHomePage();
+        loginPage.clickOnLogOutButton();
+    }
+
+
+    @Test
+    @Order(1)
+    public void addValidComment_when_postCommentButtonClicked() {
+        postsPage.navigateToPage();
+        //postsPage.clickOnExplorePostButton(usernameRandom);
+        postsPage.clickOnExplorePostButton(usernameRandom);
+        commentsPage.writeComment();
+        commentsPage.clickOnPostCommentButton();
+//        commentsPage.validateCommentCreated();
+    }
+
+    @Test
+
+    public void editComment_when_validTextAdded() {
+
+        homePage.navigateToLatestPosts();
+        //postsPage.clickOnExplorePostButton(usernameRandom);
+        postsPage.clickOnExplorePostButton(usernameRandom);
+        commentsPage.clickOnShowCommentsButton();
+        commentsPage.clickEditComment();
+        commentsPage.editComment();
+        commentsPage.validateCommentEdited();
+    }
+
+    @Test
+
+    public void likeComment_when_likedButtonClicked() {
+        homePage.navigateToLatestPosts();
+        //postsPage.clickOnExplorePostButton(usernameRandom);
+        postsPage.clickOnExplorePostButton(usernameRandom);
+        commentsPage.clickOnShowCommentsButton();
+        commentsPage.clickOnLikeButton();
+        commentsPage.validateCommentLiked();
+    }
+
+    @Test
+
+    public void deleteComment_when_deleteButtonClicked() {
+        homePage.navigateToLatestPosts();
+        //postsPage.clickOnExplorePostButton(usernameRandom);
+        postsPage.clickOnExplorePostButton(usernameRandom);
+        commentsPage.clickOnShowCommentsButton();
+        commentsPage.clickDeleteComment();
+        commentsPage.deleteComment();
+        commentsPage.validateCommentDeleted();
+    }
+
+
+}
