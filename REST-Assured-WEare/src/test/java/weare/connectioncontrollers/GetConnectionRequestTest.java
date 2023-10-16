23b660099bf2a4d6b8462e3b3730ea2459f531f1
BorisYurukov <133022686+BorisYurukov@users.noreply.github.com>
2023-10-16 11:25:26 +0300

    I need to pull.

--- REST-Assured-WEare/src/test/java/weare/connectioncontrollers/GetConnectionRequestTest.java
+++ REST-Assured-WEare/src/test/java/weare/connectioncontrollers/GetConnectionRequestTest.java
@@ -19,7 +19,7 @@ public class GetConnectionRequestTest extends BaseTest {
     @Test
     @Label("Jira - FPW-244")
     public void getConnectionRequestTest_When_RequestSent() {
-        if (isNull(isConnectionSend )) {
+        if (isConnectionSend==false ) {
             sendConnectionRequest();
         }
         baseURI = format("%s%s", BASE_URL, format(GET_REQUEST, userReceivingRequestId));
@@ -28,6 +28,7 @@ public class GetConnectionRequestTest extends BaseTest {
                 .contentType(ContentType.JSON)
                 .when()
                 .get(baseURI);
+        System.out.println(response.getBody().asPrettyString());
 
         connectionId = response.getBody().jsonPath().getString("[0].id");
 
