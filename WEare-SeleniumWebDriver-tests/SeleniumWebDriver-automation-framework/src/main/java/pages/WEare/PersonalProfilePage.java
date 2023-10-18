dfdd81de81cdcdb7889392a9decb826ea46a0da3
Aneliya Boneva <132662622+AneliyaYaYaYa@users.noreply.github.com>
2023-10-18 16:06:15 +0300

    Selenium - editProfile validation amendments

--- WEare-SeleniumWebDriver-tests/SeleniumWebDriver-automation-framework/src/main/java/pages/WEare/PersonalProfilePage.java
+++ WEare-SeleniumWebDriver-tests/SeleniumWebDriver-automation-framework/src/main/java/pages/WEare/PersonalProfilePage.java
@@ -1,10 +1,8 @@
 package pages.WEare;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
-
-import java.text.SimpleDateFormat;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.opentest4j.AssertionFailedError;
 
 import static com.telerikacademy.testframework.Utils.LOGGER;
 import static java.lang.String.format;
@@ -30,21 +28,20 @@ public class PersonalProfilePage extends WEareBasePage {
         actions.waitForElementPresent("personalProfilePageFirstNameField");
         actions.clearingFiled("personalProfilePageFirstNameField");
         actions.typeValueInField(name, "personalProfilePageFirstNameField");
-        LOGGER.info(format("First name was set to : \"%s\"",name));
+        LOGGER.info(format("First name was set to : \"%s\"", name));
     }
 
     public void fillUpLastNameField(String lastName) {
         actions.waitForElementPresent("personalProfilePageLastNameField");
         actions.clearingFiled("personalProfilePageLastNameField");
         actions.typeValueInField(lastName, "personalProfilePageLastNameField");
-        LOGGER.info(format("Last name was set to : \"%s\"",lastName));
+        LOGGER.info(format("Last name was set to : \"%s\"", lastName));
     }
 
     public void fillBirthdayField(String date) {
         actions.waitForElementPresent("personalProfilePageBirthdayField");
-//        actions.clearingFiled("personalProfilePageBirthdayField");
         actions.typeValueInField(date, "personalProfilePageBirthdayField");
-        LOGGER.info(format("Birthday date was set to : \"%s\"",date));
+        LOGGER.info(format("Birthday date was set to : \"%s\"", date));
     }
 
     public void changeGender(String genderType) {
@@ -53,7 +50,7 @@ public class PersonalProfilePage extends WEareBasePage {
 
         actions.waitForElementPresent("personalProfilePageGenderButton", genderType);
         actions.clickElement("personalProfilePageGenderButton", genderType);
-        LOGGER.info(format("Gender was changed to : \"%s\"",genderType));
+        LOGGER.info(format("Gender was changed to : \"%s\"", genderType));
     }
 
     public void changeEmail(String newEmail) {
@@ -61,7 +58,7 @@ public class PersonalProfilePage extends WEareBasePage {
         actions.clearingFiled("personalProfilePageEmailField");
 
         actions.typeValueInField(newEmail, "personalProfilePageEmailField");
-        LOGGER.info(format("Email date was changed to : \"%s\"",newEmail));
+        LOGGER.info(format("Email date was changed to : \"%s\"", newEmail));
     }
 
     public void fillSelfDescriptionField(String description) {
@@ -93,7 +90,7 @@ public class PersonalProfilePage extends WEareBasePage {
         actions.clickElement("personalProfileUpdatePage.ProfessionalCategoryDropDown", profession);
         actions.waitForElementClickable("personalProfileUpdatePage.PersonalProfileUpdateCategoryButton");
         actions.clickElement("personalProfileUpdatePage.PersonalProfileUpdateCategoryButton");
-        LOGGER.info(format("Profession was changed to : \"%s\"",profession));
+        LOGGER.info(format("Profession was changed to : \"%s\"", profession));
     }
 
     public void changeServices(String service, String weeklyAvailability) {
@@ -106,7 +103,6 @@ public class PersonalProfilePage extends WEareBasePage {
         actions.clickElement("personalProfileUpdatePage.PersonalProfileUpdateServiceButton");
 
     }
-
     public void assertFirstNameErrorMessage() {
         actions.assertElementText("personalProfilePageEditErrorMessage", "first name must have at least 3 symbols!");
     }
@@ -114,20 +110,77 @@ public class PersonalProfilePage extends WEareBasePage {
     public void assertLastNameErrorMessage() {
         actions.assertElementText("personalProfilePageEditErrorMessage", "last name must have at least 3 symbols!");
     }
-    public void assertSelfDescription(String selfDescriptionText) {
-        actions.assertElementText("personalProfilePageSelfDescriptionField", selfDescriptionText);
+
+    public void validateSuccessfulEdit(String firstName, String lastName) {
+        try {
+            actions.assertElementAttribute("personalProfilePageFirstNameField", "value", firstName);
+            actions.assertElementAttribute("personalProfilePageLastNameField", "value", lastName);
+            LOGGER.info("User personal information updated successfully.");
+        } catch (AssertionFailedError e) {
+            Assertions.fail("User personal information was not updated successfully.");
+        }
     }
 
-    public void assertCity(String city) {
-        actions.assertElementText("personalProfilePageCityAssert", city);
+    public void validateEditFailed() {
+        try {
+            actions.assertElementPresent("personalProfilePageEditErrorMessage");
+            LOGGER.info("User personal information not updated successfully as invalid data passed.");
+        } catch (AssertionFailedError e) {
+            Assertions.fail("User personal information was updated successfully despite invalid data passed.");
+        }
     }
 
-    public void assertElementPresent(String locator) {
-        actions.assertElementPresent(locator);
+    public void validateSelfDescriptionUpdated(String selfDescription) {
+        try {
+            actions.assertElementText("personalProfilePageSelfDescriptionField", selfDescription);
+            LOGGER.info("User self description was updated successfully.");
+        } catch (AssertionFailedError e) {
+            Assertions.fail("User self description was not updated successfully.");
+        }
     }
 
-    public void assertElementAttribute(String locator, String attributeName, String attributeValue, Object... arguments) {
-        actions.assertElementAttribute(locator, attributeName, attributeValue, arguments);
+    public void validateGenderUpdated(String gender) {
+        try {
+            actions.assertElementAttribute("personalProfilePageGenderButton", "value", gender, gender);
+            LOGGER.info("User gender type was updated successfully.");
+        } catch (AssertionFailedError e) {
+            Assertions.fail("User gender type was not updated successfully.");
+        }
     }
 
+    public void validateEmailUpdated(String newEmail) {
+        try{
+            actions.assertElementAttribute("personalProfilePageEmailField", "value", newEmail);
+            LOGGER.info("User email was updated successfully.");
+        } catch (AssertionFailedError e) {
+            Assertions.fail("User email was not updated successfully.");
+        }
+    }
+
+    public void validateCityUpdated(String city) {
+        try{
+            actions.assertElementText("personalProfilePageCityAssert", city);
+            LOGGER.info("User city was updated successfully.");
+        } catch (AssertionFailedError e) {
+            Assertions.fail("User city was not updated successfully.");
+        }
+    }
+
+    public void validateCategoryUpdated(String profession) {
+        try{
+            actions.assertElementAttribute("personalProfilePage.professionalCategory", "textContent", profession);
+            LOGGER.info("User category profession was updated successfully.");
+        } catch (AssertionFailedError e) {
+            Assertions.fail("User category profession was not updated successfully.");
+        }
+    }
+
+    public void validateServiceUpdated() {
+        try{
+            //actions.assertElementAttribute("personalProfilePage.professionalCategory", "textContent", "");
+            LOGGER.info("User category profession was updated successfully.");
+        } catch (AssertionFailedError e) {
+            Assertions.fail("User category profession was not updated successfully.");
+        }
+    }
 }
