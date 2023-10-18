8a5f6a7b3822fc3ac7d67f0f73608c073befd332
Aneliya Boneva <132662622+AneliyaYaYaYa@users.noreply.github.com>
2023-10-18 11:08:40 +0300

    Revert "Adding postman collection with independent requirements(Postman Independent Day)"
    This reverts commit 476f2da010cece3d5c22ecc892de643710b5ea9f.

--- Postman-Collections/RunAndReport_API_Tests.bat
+++ Postman-Collections/RunAndReport_API_Tests.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Run Newman to execute Postman collection synchronously
-call newman run "WEare - Postman API Tests.postman_collection.json" -e "WEare_env NEW.postman_environment.json" --reporters cli,htmlextra
+call newman run "WEare - Postman API Tests.postman_collectionv2.json" -e "WEare_env NEW.postman_environmentv2.json" --reporters cli,htmlextra
 
 :: Find the latest generated HTML report in the newman folder and try to open it
 for /f "delims=" %%i in ('dir /b /o-d /a-d newman\*.html') do (
