b3b05cea2eb4e3333df7856184ea4b3d49a5d205
Aneliya Boneva <132662622+AneliyaYaYaYa@users.noreply.github.com>
2023-10-18 11:41:55 +0300

    restoring Allure reports on RestAssured

---
+++ REST-Assured-WEare/RunAndReport_REST_Assured_API-Tests_unhappy.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Current directory: %cd%
+
+set SUITE_PATH=src\test\resources\unhappy-suite.xml
+set RESULTS_PATH=.\allure-results
+
+:: Clear old Allure results
+if exist %RESULTS_PATH%\* (
+    echo Deleting old Allure results...
+    del /Q /F %RESULTS_PATH%\*
+)
+
+:: Run unhappy-suite.xml using Maven with surefire.suiteXmlFiles
+call mvn clean test -Dsurefire.suiteXmlFiles=%SUITE_PATH%
+
+:: Serve the Allure report from the allure-results directory
+call npx allure-commandline serve %RESULTS_PATH%
