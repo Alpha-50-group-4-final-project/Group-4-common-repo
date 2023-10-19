74c8f4c75fa6635c6d8a1a79d6504752b0e0b763
Aneliya Boneva <132662622+AneliyaYaYaYa@users.noreply.github.com>
2023-10-19 13:39:05 +0300

    amendments on postTest

--- WEare-SeleniumWebDriver-tests/SeleniumWebDriver-automation-framework/src/test/java/WEare/privatepart/PostsTests.java
+++ WEare-SeleniumWebDriver-tests/SeleniumWebDriver-automation-framework/src/test/java/WEare/privatepart/PostsTests.java
@@ -26,7 +26,7 @@ public class PostsTests extends BaseTest {
     @Tag("HappyPath")
     @DisplayName("Create a New Public Post")
     public void createPublicPost_when_addNewPostButtonClicked() {
-        homePage.navigateToLatestPosts();
+        postsPage.navigateToPage();
         postsPage.clickAddNewPost();
         postsPage.selectPostVisibility();
         postsPage.writePostMessage();
@@ -40,7 +40,7 @@ public class PostsTests extends BaseTest {
     @DisplayName("Validate the User Can Edit His Public Post")
     public void editPublicPost_when_editPostButtonClicked() {
         apiPost = api.createPost(usernameRandom, passwordRandom, postBody);
-        homePage.navigateToLatestPosts();
+        postsPage.navigateToPage();
         postsPage.explorePost(usernameRandom);
         postsPage.clickEditPost();
         postsPage.selectPostVisibility();
@@ -56,7 +56,7 @@ public class PostsTests extends BaseTest {
     @DisplayName("Validate User Can Like a Public Post")
     public void likePublicPost_when_likeButtonClicked() {
         apiPost = api.createPost(usernameRandom, passwordRandom, postBody);
-        homePage.navigateToLatestPosts();
+        postsPage.navigateToPage();
         postsPage.likePostByUsername(usernameRandom);
         postsPage.validatePostLiked();
         api.deletePost(usernameRandom, passwordRandom, apiPost.postId);
@@ -69,7 +69,7 @@ public class PostsTests extends BaseTest {
     public void dislikePublicPost_when_dislikeButtonClicked() {
         apiPost = api.createPost(usernameRandom, passwordRandom, postBody);
         api.likePost(usernameRandom, passwordRandom, apiPost.postId);
-        homePage.navigateToLatestPosts();
+        postsPage.navigateToPage();
         postsPage.dislikePostByUsername(usernameRandom);
         postsPage.validatePostDisliked();
         api.deletePost(usernameRandom, passwordRandom, apiPost.postId);
@@ -81,7 +81,7 @@ public class PostsTests extends BaseTest {
     @DisplayName("Validate User Can Delete His Public PostValidate User Can Delete His Public Post")
     public void deletePost_when_deleteButtonClicked() {
         api.createPost(usernameRandom, passwordRandom, postBody);
-        homePage.navigateToLatestPosts();
+        postsPage.navigateToPage();
         postsPage.explorePost(usernameRandom);
         postsPage.clickDeletePost();
         postsPage.confirmDeletion();
